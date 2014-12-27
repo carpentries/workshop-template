2bc3bb0628ac1a5e6e2102a0788e6fb12ca14751
Raniere Silva <raniere@ime.unicamp.br>
2014-12-26 22:29:18 -0200

    Add language field to YAML header
    Know the language that our instructors are using should be
    important.

--- tools/check.py
+++ tools/check.py
@@ -123,6 +123,25 @@ COUNTRIES = [
     'Zimbabwe'
 ]
 
+LANGUAGES = [
+    'aa', 'ab', 'ae', 'af', 'ak', 'am', 'an', 'ar', 'as', 'av', 'ay', 'az',
+    'ba', 'be', 'bg', 'bh', 'bi', 'bm', 'bn', 'bo', 'br', 'bs', 'ca', 'ce',
+    'ch', 'co', 'cr', 'cs', 'cu', 'cv', 'cy', 'da', 'de', 'dv', 'dz', 'ee',
+    'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'ff', 'fi', 'fj', 'fo', 'fr',
+    'fy', 'ga', 'gd', 'gl', 'gn', 'gu', 'gv', 'ha', 'he', 'hi', 'ho', 'hr',
+    'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'io', 'is',
+    'it', 'iu', 'ja', 'jv', 'ka', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn',
+    'ko', 'kr', 'ks', 'ku', 'kv', 'kw', 'ky', 'la', 'lb', 'lg', 'li', 'ln',
+    'lo', 'lt', 'lu', 'lv', 'mg', 'mh', 'mi', 'mk', 'ml', 'mn', 'mr', 'ms',
+    'mt', 'my', 'na', 'nb', 'nd', 'ne', 'ng', 'nl', 'nn', 'no', 'nr', 'nv',
+    'ny', 'oc', 'oj', 'om', 'or', 'os', 'pa', 'pi', 'pl', 'ps', 'pt', 'qu',
+    'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sc', 'sd', 'se', 'sg', 'si', 'sk',
+    'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st', 'su', 'sv', 'sw', 'ta',
+    'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw',
+    'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'wo', 'xh', 'yi',
+    'yo', 'za', 'zh', 'zu'
+]
+
 
 def add_error(msg, errors):
     """
@@ -154,7 +173,6 @@ def add_suberror(msg, errors):
     """
     errors.append("\t{0}".format(msg))
 
-
 def look_for_fixme(func):
     '''Decorator to see whether a value starts with FIXME.'''
     def inner(arg):
@@ -181,6 +199,11 @@ def check_country(country):
     '''A valid country is in the list of recognized countries.'''
     return country in COUNTRIES
 
+
+@look_for_fixme
+def check_language(language):
+    """A valid language is a ISO 639-1 code."""
+    return language in LANGUAGES
 
 @look_for_fixme
 def check_humandate(date):
@@ -281,6 +304,9 @@ HANDLERS = {
     'country':    (True, check_country,
                    'country invalid: must use full hyphenated name from: ' +
                    ' '.join(COUNTRIES)),
+
+    'language' :  (False,  check_language,
+                   'language invalid: must be a ISO 639-1 code'),
 
     'humandate':  (True, check_humandate,
                    'humandate invalid. Please use three-letter months like ' +
