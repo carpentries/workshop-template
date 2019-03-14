4c6c155082866757a18e1abd558d0ca2698be1b9
Brian Ballsun-Stanton <brian@fedarch.org>
2019-03-14 17:25:41 +1100

    Updating Travis to python3.6, and pyyaml checker
    Made travis 3.6 instead of 3.2..4, so that modern versions of ubuntu can run the files without problem. Gawk doesn't work in 14.04 with the inplace extension, so wrote a python script to do the same thing

---
+++ bin/_travis.py
@@ -0,0 +1,32 @@
+import yaml
+import datetime
+# We need to split the incoming file
+with open("index.md", 'r') as reader:
+	# There are multiple yaml documents in the index.md	
+
+	blankbit, yaml_to_load, text = reader.read().split("---")
+
+yaml_data = yaml.load(yaml_to_load, Loader=yaml.Loader) # To deal with depreciation warnings, we need Loader=Loader
+
+	
+yaml_data['carpentry']="swc" 
+yaml_data['venue']="Foo" 
+yaml_data['address']="Room 123" 
+yaml_data['country']="us" 
+yaml_data['language']="en" 
+yaml_data['latlng']="0,0" 
+yaml_data['humandate']="Jan 01-02, 2020" 
+yaml_data['humantime']="9:00 am - 4:30 pm" 
+yaml_data['startdate']= datetime.datetime.today()
+yaml_data['enddate']= datetime.datetime.today()
+yaml_data['instructor']=["Foo"]
+yaml_data['helper']=["Foo"] 
+yaml_data['email']=["foo@bar.com"]
+yaml_data['collaborative_notes'] =  "http://foo.bar"
+yaml_data['eventbrite']="1234567890AB" 
+
+with open("index.md", "w") as writer:
+	writer.write("---\n") #blankbit is technically a yaml document
+	yaml.dump(yaml_data, writer)
+	writer.write("---\n")
+	writer.write(text)
